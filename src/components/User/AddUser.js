ed6715db63d7aab38417e028a95e888a6e32a8c7
Vaishakh KP <kpvaisakh@gmail.com>
2023-07-04 14:56:13 +0530

    Introduced ref for accessing input through current value object

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,47 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErronModal from '../UI/ErrorModal';
 import Wrapper from '../Helpers/Wrapper';
 
-const inputDefalutState = {
-  username: '',
-  age: '',
-};
 const AddUser = (props) => {
-  const [userData, setUserdata] = useState(inputDefalutState);
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (
-      userData.username.trim().length === 0 ||
-      userData.age.trim().length === 0
-    ) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    //console.log(enteredName, enteredUserAge);
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values)',
       });
       return;
     }
-    if (+userData.age < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0)',
       });
       return;
     }
-    setUserdata(inputDefalutState);
+    //console.log(userData);
+    const userData = { username: enteredName, age: enteredUserAge };
     //console.log(userData);
     props.onAddUser(userData);
   };
 
-  const inputHandler = (input, value) => {
-    setUserdata((prevInput) => {
-      return { ...prevInput, [input]: value };
-    });
-  };
   const errorHandler = () => {
     setError(null);
   };
@@ -57,19 +51,9 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
-          <input
-            type='text'
-            id='username'
-            value={userData.username}
-            onChange={(event) => inputHandler('username', event.target.value)}
-          ></input>
+          <input type='text' id='username' ref={nameInputRef}></input>
           <label htmlFor='age'>Age (Years)</label>
-          <input
-            type='number'
-            id='age'
-            value={userData.age}
-            onChange={(event) => inputHandler('age', event.target.value)}
-          ></input>
+          <input type='number' id='age' ref={ageInputRef}></input>
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
